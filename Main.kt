1c2e7a3b5bfe7d6c1de541abe19c15e125955699
Fancy-Gichuru <gichuruufancy@gmail.com>
2024-02-08 16:33:11 +0300

    first addition

--- Main.kt
+++ Main.kt
@@ -1,14 +1,14 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main(){
 
-    val name: String = "Stacy Wanjiru"
-    println(name)
-    var age: Byte = 18
-    println(age)
-    var phoneNumber: String = "0726990033"
-    println(phoneNumber)
-    var weight: Double = 45.4
-    println(weight)
-    var citizen: Boolean = true
-    println(true)
+val name: String = "Stacy Wanjiru"
+println(name)
+var age: Byte = 18
+println(age)
+var phoneNumber: String = "0726990033"
+println(phoneNumber)
+var weight: Double = 45.4
+println(weight)
+var citizen: Boolean = true
+println(true)
+
 }
